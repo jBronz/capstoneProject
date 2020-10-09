9b09a2077b2ebd4be4b340dfec626d2dbfe572a1
JPB <jbronzi02@rams.shepherd.edu>
2020-10-09 13:47:51 -0400

    added alan ai to code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
+const alanKey = '6947ae0effb1401bcafdcca39b0351bb2e956eca572e1d8b807a3e2338fdd0dc/stage';
 function App() {
+  useEffect(()=> {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({command}) => {
+        if(command === 'testCommand') {
+            alert('This code works');
+
+        }
+
+      }
+
+    })
+  },[])
   return (
     <div className="App">
       <header className="App-header">
