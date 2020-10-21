f715a7e481e035b4a55468d5e6c118b7c9848136
nguyeale27 <nguyenalex927@gmail.com>
2020-10-21 14:41:56 -0400

    Added all 5 original output languages to switch

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import alanBtn from '@alan-ai/alan-sdk-web';
 
 const alanKey = '6947ae0effb1401bcafdcca39b0351bb2e956eca572e1d8b807a3e2338fdd0dc/stage';
 function App() {
+  const instruction = "Click the icon on the bottom right to start recording.";
   const [statementTranslation, startTrans] = useState([]);
   const[endLanguage, secondLang] = useState([]);
   const[endTranslation, secondTrans] = useState([]);
-  const langs = {"de":"German", "en": "English"};
+  //const langs = {"de":"German", "en": "English"};
 ;  useEffect(()=> {
     alanBtn({
       key: alanKey,
       onCommand: ({command, text, response,lang}) => {
         if(command === 'testCommand') {
             alert('This code works');
-
+            
         }
         if(command ==='sendData'){
+          
           startTrans(text);
           switch(lang){
             case "de":
@@ -25,6 +27,15 @@ function App() {
               break;
               case "fr":
                 secondLang("French");
+                break;
+                case "ru":
+                  secondLang("Russian");
+                break;
+                case "es":
+                  secondLang("Spanish");
+                break;
+                case "en":
+                  secondLang("English");
                 break;
           }
           secondTrans(response.translated);
@@ -39,6 +50,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <a>
+          {instruction}
           {statementTranslation}
         </a>
         <a>
