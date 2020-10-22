3618ff9c993176d295f02271af85918fa22f47da
taniahdez <therna01@rams.shepherd.edu>
2020-10-21 23:21:23 -0400

    figma changes

--- src/App.js
+++ src/App.js
@@ -47,9 +47,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <a>
-          
           {statementTranslation}
         </a>
         <a>
